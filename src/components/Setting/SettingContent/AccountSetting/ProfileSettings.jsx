668ed77bd3cf9ccb34eb9dbc6299dfde9daaea17
takyujin <tyujin@naver.com>
2025-08-09 16:49:11 +0900

    프로필 사진 설정

--- src/components/Setting/SettingContent/AccountSetting/ProfileSettings.jsx
+++ src/components/Setting/SettingContent/AccountSetting/ProfileSettings.jsx
@@ -15,45 +15,63 @@ function ProfileSettings() {
 
   return (
     <div className="ProfileSettings-container">
-      {/* 왼쪽 영역 */}
-      <div className="ProfileSettings-left-section">
-        <div className="ProfileSettings-profile-img">
-          <div className="ProfileSettings-gray-circle"></div>
+
+      <div className="ProfileSettings-top-section">
+        <div className="ProfileSettings-left-top-section">
+          <div className="ProfileSettings-input-row half-width">
+            <label>이름</label>
+            <input type="text" placeholder="김철수" />
+          </div>
+          <div className="ProfileSettings-input-row half-width">
+            <label>나이</label>
+            <input type="text" placeholder="27" />
+          </div>
+          <div className="ProfileSettings-input-row half-width">
+            <label>성별</label>
+            <input type="text" placeholder="남성" />
+          </div>
         </div>
 
-        <div className="ProfileSettings-input-row">
-          <label>이름</label>
-          <input type="text" placeholder="김철수" />
+        <div className="ProfileSettings-profile-photo">
+          <img
+            src="/images/ptf.png"
+            alt="프로필 사진"
+            className="ProfileSettings-photo-img"
+          />
         </div>
-        <div className="ProfileSettings-input-row">
-          <label>나이</label>
-          <input type="text" placeholder="27" />
-        </div>
-        <div className="ProfileSettings-input-row">
-          <label>성별</label>
-          <input type="text" placeholder="남성" />
-        </div>
+      </div>
+
+      <div className="ProfileSettings-left-section">
         <div className="ProfileSettings-input-row">
           <label>전화번호</label>
-          <input type="text" placeholder="010-1234-5678" className="ProfileSettings-num" />
-          <span className="ProfileSettings-error-text">현재 전화번호 인증이 완료되지 않았습니다.</span>
+          <input
+            type="text"
+            placeholder="010-1234-5678"
+            className="ProfileSettings-num"
+          />
+          <span className="ProfileSettings-error-text">
+            현재 전화번호 인증이 완료되지 않았습니다.
+          </span>
         </div>
         <div className="ProfileSettings-input-row">
           <label>메일</label>
-          <input type="text" placeholder="asdf12345@naver.com" className="ProfileSettings-num" />
-          <span className="ProfileSettings-error-text">현재 이메일 인증이 완료되지 않았습니다.</span>
+          <input
+            type="text"
+            placeholder="asdf12345@naver.com"
+            className="ProfileSettings-num"
+          />
+          <span className="ProfileSettings-error-text">
+            현재 이메일 인증이 완료되지 않았습니다.
+          </span>
+        </div>
         <div className="ProfileSettings-input-row">
           <label>주소</label>
           <input type="text" placeholder="충청남도 아산시 어쩌고 저쩌고" />
         </div>
-        </div>
       </div>
 
-      
-
-        <button className="ProfileSettings-submit-button">등록</button>
-      </div>
-
+      <button className="ProfileSettings-submit-button">등록</button>
+    </div>
   );
 }
 
